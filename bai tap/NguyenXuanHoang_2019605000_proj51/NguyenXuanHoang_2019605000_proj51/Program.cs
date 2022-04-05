0b60fd10d9d451ce5fa6df99dceea2bdf5fda61c
Xuân Hoàng <xhoang0509@gmail.com>
2022-04-05 10:40:34 +0700

    proj51

---
+++ bai tap/NguyenXuanHoang_2019605000_proj51/NguyenXuanHoang_2019605000_proj51/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NguyenXuanHoang_2019605000_proj51
+{
+    class Program
+    {
+        // static ThiSinh[] dsTS;
+        static List<ThiSinh> dsTS = new List<ThiSinh>();
+
+        static void nhap()
+        {
+            ThiSinh temp = new ThiSinh();
+            temp.nhap();
+            dsTS.Add(temp);
+        }
+
+        static void xuat()
+        {
+            ThiSinh.inTieuDe();
+            foreach (ThiSinh item in dsTS)
+            {
+                item.xuat();
+            }
+        }
+
+        // hien thi theo tong diem tang dan
+        static void hienThiTheoTongDiem()
+        {
+            dsTS.Sort();
+
+        }
+        static void hienThiTheoDiaChi()
+        {
+            dsTS.Sort(new CompareAddress());
+        }
+
+        static void timKiemSBD()
+        {
+            string sdb;
+            Console.Write("Nhap so bao danh can tim: ");
+            sdb = Console.ReadLine();
+            ThiSinh x = dsTS.Find(x => x.soBD.Contains(sdb));
+
+            if(x  != null)
+            {
+                Console.WriteLine("\nTim thay");
+                ThiSinh.inTieuDe();
+                x.xuat();
+            } else
+            {
+                Console.WriteLine("Khong tim thay");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int chon;
+            do
+            {
+                Console.WriteLine("\n1. Nhap 1 thi tinh ");
+                Console.WriteLine("2. Hien thi danh sach");
+                Console.WriteLine("3. Hien thi danh sach theo tong diem");
+                Console.WriteLine("4. Hien thi danh sach theo dia chi");
+                Console.WriteLine("5. Tim kiem theo so bao danh");
+                Console.WriteLine("6. Ket thuc chuong trinh");
+                Console.Write("Nhap lua chon: ");
+                chon = int.Parse(Console.ReadLine());
+
+                switch (chon)
+                {
+                    case 1:
+                        {
+                            nhap();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("\n\t\tDANH SACH VUA NHAP LA");
+                            xuat();
+                            break;
+                        }
+                    case 3:
+                        {
+                            hienThiTheoTongDiem();
+                            Console.WriteLine("\n\t\tDANH SACH SAP XEP THEO TONG DIEM LA");
+                            xuat();
+                            break;
+                        }
+                    case 4:
+                        {
+                            hienThiTheoDiaChi();
+                            Console.WriteLine("\n\t\tDANH SACH SAP XEP THEO DIA CHI LA");
+                            xuat();
+                            break;
+                        }
+                    case 5:
+                        {
+                            timKiemSBD();                            
+                            break;
+                        }
+                }
+            } while (chon != 6);
+        }
+    }
+}
---
+++ bai tap/NguyenXuanHoang_2019605000_proj51/NguyenXuanHoang_2019605000_proj51/ThiSinh.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NguyenXuanHoang_2019605000_proj51
+{
+    class ThiSinh : IComparable
+    {
+        public string soBD;
+        private string hoTen;
+        public string diaChi;
+        float toan, ly, hoa, diemUT;
+
+        public void nhap()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("\nNhap so bao danh: ");
+                    soBD = Console.ReadLine();
+                    Console.Write("Nhap ho ten: ");
+                    hoTen = Console.ReadLine();
+                    Console.Write("Nhap dia chi: ");
+                    diaChi = Console.ReadLine();
+                    Console.Write("Nhap diem toan: ");
+                    toan = float.Parse(Console.ReadLine());
+                    Console.Write("Nhap diem ly: ");
+                    ly = float.Parse(Console.ReadLine());
+                    Console.Write("Nhap diem hoa: ");
+                    hoa = float.Parse(Console.ReadLine());
+                    Console.Write("Nhap diem uu tien: ");
+                    diemUT = float.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("CO LOI, NHAP LAI " + e.Message);
+                }
+            }
+        }
+
+        public float tongDiem()
+        {
+            return toan + ly + hoa + diemUT;
+        }
+
+        public static void inTieuDe()
+        {
+            string kq = String.Format("{0,-15}{1,-20}{2,-15}{3,8}{4,8}{5,8}{6,8}{7,8}",
+                "So bao danh", "Ho ten", "Dia Chi", "Toan", "Ly", "Hoa", "Diem UT", "Diem TK");
+            Console.WriteLine(kq);
+        }
+
+        public void xuat()
+        {
+            string kq = String.Format("{0,-15}{1,-20}{2,-15}{3,8}{4,8}{5,8}{6,8}{7,8}",
+              soBD, hoTen, diaChi, toan, ly, hoa, diemUT, tongDiem());
+            Console.WriteLine(kq);
+        }
+
+        public int CompareTo(object obj)
+        {
+            ThiSinh temp = (ThiSinh)obj;
+            return (int)(this.tongDiem() - temp.tongDiem());
+        }
+    }
+
+    class CompareAddress : IComparer<ThiSinh>
+    {
+
+        public int Compare(ThiSinh x, ThiSinh y)
+        {
+            return (x.diaChi.CompareTo(y.diaChi));
+        }
+    }
+}
